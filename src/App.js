4dfe4b3d07ffc0395d80327d471cfca98396efa5
周泽飞 <zefei.zhou@tongdun.net>
2021-01-12 15:22:55 +0800

    去掉readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
+import "./App.css"
 
-function App() {
+const itemH = 30;
+const totalData = []
+for (let i = 0;i< 1000; i++ ){
+  totalData.push({
+    key: i.toString(),
+    val: 'Item' + i,
+  })
+};
+
+export default () => {
+  const [data,setData] = useState([]);
+  const [transform,setTransform] = useState('');
+  const scrollWrap = useRef()
+
+  useEffect(()=>{
+    updateViewContent();
+  },[]);
+
+  // 更新
+  const updateViewContent = (scrollTop = 0) => {
+    const viewCount = Math.ceil(scrollWrap.current.clientHeight/itemH); // 可视区域总的展示个数 进1位
+    const start = Math.floor(scrollTop/itemH); // 舍去小数点
+    // star 四舍五入
+    const roundVal = Math.round(scrollTop/itemH) - start; // 查看差异值
+    const end = start + viewCount + roundVal;
+    const viewData = totalData.slice(start,end);
+    setData(viewData);
+    setTransform(`translate3d(0, ${ start * itemH }px, 0)`)
+  };
+
+  // 滚动
+  const handleScroll = (e) => {
+    updateViewContent(e.target.scrollTop)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="virtual-list" onScroll={handleScroll} ref={scrollWrap}>
+        <div className="virtual-list-height" style={{"height": (totalData.length * itemH) + 'px'}}></div>
+        <div className="view-content" style={{transform:transform}}>
+          {
+            data.map(item=>{
+              return (
+                <div className="view-item" key={item.key}>{item.val}</div>
+              )
+            })
+          }
+        </div>
     </div>
-  );
+  )
 }
-
-export default App;
